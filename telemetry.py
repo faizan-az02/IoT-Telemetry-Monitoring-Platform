8707d30bd8a35c8037ea618682e259e813134f98
Faizan Aziz <faizan.professional02@gmail.com>
2026-01-28 01:58:54 +0500

    feat: telemetry data collection module

---
+++ telemetry.py
@@ -0,0 +1,40 @@
+import os
+import json
+import psutil
+import time
+from datetime import datetime
+
+# Getting Device Information
+
+dataset_size = int(input("Enter the dataset size you want to get: "))
+time_interval = int(input("Enter the time interval between the samples: "))
+
+device_id = os.getenv('DEVICE_ID', 'edge-1')
+
+print(f"Device ID: {device_id}")
+
+with open("telemetry_data.txt", 'a') as f:
+
+    for i in range(dataset_size):
+        
+        # Collecting Telemetry Data
+        telemetry_data = {
+            "device_id": device_id,
+            "cpu_usage (%)": psutil.cpu_percent(interval=1),
+            "memory_usage (%)": psutil.virtual_memory().percent,
+            "disk_usage (%)": psutil.disk_usage('/').percent,
+            "timestamp": datetime.now().strftime("%H:%M:%S")
+        }
+
+        # Writing Telemetry Data to File
+        json.dump(telemetry_data, f)
+        f.write('\n')
+
+        time.sleep(time_interval)
+
+        print(f"Sample {i+1}/{dataset_size} collected.", end = '\r')
+
+print("\nTelemetry data collection complete.")
+              
+
+    
